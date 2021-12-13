e588593d21b9fb1e53b8b36f4bc1aecde12fdf23
Bocheng Yang <bocheng.yang@instill.tech>
2021-12-13 23:25:27 +0800

    feat: init commit

---
+++ cmd/helloworld/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/bochengyang/grpc-gateway-prac/pkg/helloworld"
+	"google.golang.org/grpc"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
+	log.Printf("Received: %v", in.Name)
+	return &helloworld.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+const (
+	port = ":50001"
+)
+
+func main() {
+	// Create gRPC Server
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	log.Println("gRPC server is running.")
+	helloworld.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
---
+++ pkg/gateway/gateway.go
@@ -0,0 +1,25 @@
+package gateway
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+
+	"github.com/bochengyang/grpc-gateway-prac/pkg/helloworld"
+)
+
+func New(ctx context.Context, endpoint string) (http.Handler, error) {
+	gw := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+
+	if err := helloworld.RegisterGreeterHandlerFromEndpoint(ctx, gw, endpoint, opts); err != nil {
+		return nil, err
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", gw)
+
+	return mux, nil
+}
